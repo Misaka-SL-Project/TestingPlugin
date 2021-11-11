b2e4d7667b138c775bfc61219de06ed644e4c290
Misaka_ZeroTwo <zhaguanyang@hotmail.com>
2021-11-11 11:11:42 +0800

    stuffs

---
+++ EventHandlers/ServerEventHandlers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandlers
+{
+    public class ServerEventHandlers
+    {
+        public static void OnEnabled()
+        {
+
+        }
+        public static void OnDisabled()
+        {
+
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+
+namespace TestingPlugin
+{
+    public class TestingPlugin : Plugin<Config>
+    {
+        public override void OnEnabled()
+        {
+            EventHandlers.PlayerEventHandlers.OnEnabled();
+            EventHandlers.ServerEventHandlers.OnEnabled();
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            EventHandlers.PlayerEventHandlers.OnDisabled();
+            EventHandlers.ServerEventHandlers.OnDisabled();
+            base.OnDisabled();
+
+        }
+    }
+}
